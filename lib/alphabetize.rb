e824280d213f9f54aab1929b5ae32b36a7845fed
Redmond Chan <redmondchan92@gmail.com>
2019-01-07 04:27:45 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,8 +1,8 @@
 def alphabetize(arr)
   esp_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |str|
-    str.split('').mapter
+     str.split('').map do |letter|
     esp_alphabet.index(letter)
   end
-end# code here
+end
 end
